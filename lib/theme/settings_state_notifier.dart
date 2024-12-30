02408f2b4a00799e333501e23d64dda14acf9274
Haden Hiles <hi@hadenhiles.com>
2024-12-30 16:03:41 -0500

    Attempt to make app settings persist correctly between sessions (dark_mode in particular)

--- lib/theme/settings_state_notifier.dart
+++ lib/theme/settings_state_notifier.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:skilldrills/models/settings.dart';
 
 class SettingsStateNotifier extends ChangeNotifier {
-  Settings settings = Settings(
-    true,
-    (ThemeMode.system == ThemeMode.dark),
-  );
+  Settings _settings = Settings(false, false); // Initialize with defaults
 
-  void updateSettings(Settings settings) {
-    this.settings = settings;
-    notifyListeners();
+  Settings get settings => _settings;
+
+  Future<void> updateSettings(Settings newSettings) async {
+    _settings = newSettings;
+    notifyListeners(); // Notify listeners of the change
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('vibrate', _settings.vibrate);
+    await prefs.setBool('dark_mode', _settings.darkMode);
   }
 }
